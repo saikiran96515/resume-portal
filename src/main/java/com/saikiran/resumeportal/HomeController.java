e0f4bdad504e961167bb2cc5d9c978ff4a4a722a
saikiran96515 <saikiran96515@gmail.com>
2022-08-08 12:39:24 +0530

    Added the Responsibilities list

--- src/main/java/com/saikiran/resumeportal/HomeController.java
+++ src/main/java/com/saikiran/resumeportal/HomeController.java
@@ -28,7 +28,11 @@ public class HomeController {
         job1.setCompany("Company1");
         job1.setDesignation("Designation");
         job1.setStartDate(LocalDate.of(2020,1,1));
-        //job1.setEndDate(LocalDate.of(2020,3,3));
+        List<String>  MyResponsibilities = new ArrayList<>();
+        MyResponsibilities.add("Done the research on Theory of Relativity");
+        MyResponsibilities.add("Fundamental Research");
+        MyResponsibilities.add("Advance Quantum Mechanics");
+        job1.setResponsibilities(MyResponsibilities);
         job1.setCurrentJob(true);
         job1.setId(1);
         Job job2 = new Job();
@@ -37,6 +41,7 @@ public class HomeController {
         job2.setStartDate(LocalDate.of(2019,1,1));
         job2.setEndDate(LocalDate.of(2021,3,3));
         job2.setId(2);
+        job2.setResponsibilities(MyResponsibilities);
         profile1.getJobs().clear();
         profile1.getJobs().add(job1);
         profile1.getJobs().add(job2);
